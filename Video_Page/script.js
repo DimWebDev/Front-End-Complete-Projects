5eca5099fcf16218ff79e723c95b87b620baecc2
DimWebDev <77544032+DimWebDev@users.noreply.github.com>
2021-07-04 13:41:02 +0300

    Delete Video_Page directory

--- Video_Page/script.js
+++
@@ -1,7 +0,0 @@
-const toggleMenu=document.querySelector('.toggle');
-const showcase=document.querySelector('.showcase');
-
-toggleMenu.addEventListener('click', () => {
-    toggleMenu.classList.toggle('active')
-    showcase.classList.toggle('active')
-})
